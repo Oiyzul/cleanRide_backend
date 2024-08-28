a9099990bc0f3377300dffaca73e3735f0fdbad2
Waizul <waizul@gmail.com>
2024-08-28 14:45:31 +0600

    Fixed cors origin.

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,20 @@
-import express from 'express'
-import cors from 'cors'
-import router from './routes';
-import notFound from './middlewares/notFound';
-import globalErrorHandler from './middlewares/globalErrorHandler';
+import express from "express";
+import cors from "cors";
+import router from "./routes";
+import notFound from "./middlewares/notFound";
+import globalErrorHandler from "./middlewares/globalErrorHandler";
 
 const app = express();
 
 // Middleware to parse JSON request bodies
-app.use(cors())
-app.use(express.json())
+app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
+app.use(express.json());
 
 // Routes
-app.use('/api', router)
+app.use("/api", router);
 
-app.use(globalErrorHandler)
+app.use(globalErrorHandler);
 
-app.use(notFound)
+app.use(notFound);
 
-export default app
+export default app;
